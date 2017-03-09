483dc70343af5c94ab701ed0ff76f39ad80062d7
Ihor Rudenko <irudenko@softserveinc.com>
2017-03-09 15:48:56 +0200

    Add components and its styles

---
+++ app/events/event-thumbnail.component.ts
@@ -0,0 +1,16 @@
+
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'event-thumbnail',
+    templateUrl: 'app/events/event-thumbnail.component.html',
+    styles: [`
+        .pad-left { margin-left: 10px; }
+        .well div { color: #bbb; }
+    `]
+})
+export class EventThumbnailComponent {
+    @Input() eventThumbnail:any;
+
+    
+}
