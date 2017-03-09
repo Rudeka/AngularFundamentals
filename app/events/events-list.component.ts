c1ac41954c7e2a4ffde13717e9d7f402773ef380
Ihor Rudenko <irudenko@softserveinc.com>
2017-03-09 20:44:24 +0200

    Add routing for eventDetails and allEvents, createEvent and Cancel buttons

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -4,7 +4,6 @@ import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
 
 @Component({
-    selector: 'events-list',
     templateUrl: 'app/events/events-list.component.html'
 })
 export class EventsListComponent implements OnInit {
